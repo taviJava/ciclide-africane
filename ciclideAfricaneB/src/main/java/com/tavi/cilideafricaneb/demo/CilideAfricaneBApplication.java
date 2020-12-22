9bf18adf944f5222029d36c82d47c60fd5055e00
taviJava <tavi.zorila@gmail.com>
2020-12-22 15:00:11 +0200

    add project

---
+++ ciclideAfricaneB/src/main/java/com/tavi/cilideafricaneb/demo/CilideAfricaneBApplication.java
@@ -0,0 +1,13 @@
+package com.tavi.cilideafricaneb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CilideAfricaneBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CilideAfricaneBApplication.class, args);
+    }
+
+}
