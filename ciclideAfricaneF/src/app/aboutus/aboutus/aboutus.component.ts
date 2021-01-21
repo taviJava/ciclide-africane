29f2534c679132d850b78de7ab9551e4c54306f0
taviJava <tavi.zorila@gmail.com>
2021-01-21 18:33:45 +0200

    no message

--- ciclideAfricaneF/src/app/aboutus/aboutus/aboutus.component.ts
+++ ciclideAfricaneF/src/app/aboutus/aboutus/aboutus.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
-  styleUrls: ['./aboutus.component.css']
+  styleUrls: ['./aboutus.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class AboutusComponent implements OnInit {
 
-  constructor() { }
+  images = [700, 800, 807].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 2000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
 
   ngOnInit(): void {
   }
