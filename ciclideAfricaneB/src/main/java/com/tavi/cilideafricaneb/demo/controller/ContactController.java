d09e2195c995fa8d8bf90fdbb94ca0bd7213cd60
lrusanescu <lrusanescu@yahoo.com>
2021-01-22 14:29:43 +0200

    no message

---
+++ ciclideAfricaneB/src/main/java/com/tavi/cilideafricaneb/demo/controller/ContactController.java
@@ -0,0 +1,32 @@
+package com.tavi.cilideafricaneb.demo.controller;
+
+import com.tavi.cilideafricaneb.demo.persistance.dto.ContactDto;
+import com.tavi.cilideafricaneb.demo.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class ContactController {
+    @Autowired
+    private ContactService contactService;
+
+    @PostMapping("/contact")
+    public void save(@RequestBody ContactDto contactDto ){
+        contactService.save(contactDto);
+    }
+    @PutMapping("/contact")
+    public void update(@RequestBody ContactDto contactDto ){
+        contactService.update(contactDto);
+    }
+    @DeleteMapping("/contact/{id}")
+    public void delete(@PathVariable(name = "id") long id){
+        contactService.delete(id);
+    }
+    @GetMapping("/contact")
+    public List<ContactDto> getAll(){
+        return contactService.getAll();
+    }
+}
---
+++ ciclideAfricaneF/src/app/contact/model/contact.ts
@@ -0,0 +1,8 @@
+export class Contact {
+  id: number;
+  companyInformation: string;
+  address: string;
+  email: string;
+  description: string;
+  photos: any;
+}
