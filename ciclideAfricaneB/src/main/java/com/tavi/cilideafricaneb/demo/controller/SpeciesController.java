2e58ec9edecebc8f38dfc55cbf9b442fbb23f59a
lrusanescu <lrusanescu@yahoo.com>
2021-02-02 10:24:56 +0200

    no message

--- ciclideAfricaneB/src/main/java/com/tavi/cilideafricaneb/demo/controller/SpeciesController.java
+++ ciclideAfricaneB/src/main/java/com/tavi/cilideafricaneb/demo/controller/SpeciesController.java
@@ -4,7 +4,13 @@ import com.tavi.cilideafricaneb.demo.persistance.dto.SpeciesDto;
 import com.tavi.cilideafricaneb.demo.persistance.model.SpeciesModel;
 import com.tavi.cilideafricaneb.demo.service.SpeciesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -14,27 +20,51 @@ public class SpeciesController {
     private SpeciesService speciesService;
 
     @PostMapping("/admspecies")
-    public void save(@RequestBody SpeciesDto speciesDto){
+    public void save(@RequestBody SpeciesDto speciesDto) {
         speciesService.save(speciesDto);
     }
+
     @PutMapping("/admspecies")
-    public void update(@RequestBody SpeciesDto speciesDto){
+    public void update(@RequestBody SpeciesDto speciesDto) {
         speciesService.update(speciesDto);
     }
+
     @DeleteMapping("/admspecies/{id}")
-    public void delete(@PathVariable(name = "id") long id){
+    public void delete(@PathVariable(name = "id") long id) {
         speciesService.delete(id);
     }
+
     @GetMapping("/species")
-    public List<SpeciesDto> getAll(){
+    public List<SpeciesDto> getAll() {
         return speciesService.getAll();
     }
+
     @GetMapping("/find/search/{keyword}/list")
-    public List<SpeciesDto> search(@PathVariable(name = "keyword") String keyword){
+    public List<SpeciesDto> search(@PathVariable(name = "keyword") String keyword) {
         return speciesService.search(keyword);
     }
+
     @GetMapping("/species/{id}")
-    public SpeciesDto getOne(@PathVariable(name = "id") long id){
+    public SpeciesDto getOne(@PathVariable(name = "id") long id) {
         return speciesService.getOne(id);
+    }
+
+    //    @GetMapping("/species/{pagesize}/{pagenr}")
+//    public List<SpeciesDto> getAllByNameAndPage(@PathVariable(name = "pagesize") int pagesize,@PathVariable(name = "pagenr") int pagenr){
+//        return speciesService.sortAndPage(pagesize,pagenr);
+//    }
+    @GetMapping("/page/{pageNo}")
+    public String viewPage(@PathVariable(name = "pageNo") int pageNo, Model model) {
+        int pageSz = 5;
+        Page<SpeciesModel> page = speciesService.findPaginated(pageNo, pageSz);
+
+        List<SpeciesModel> listSpecies = page.getContent();
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("listProducts", listSpecies);
+
+        return "index";
     }
 }
--- ciclideAfricaneB/src/main/java/com/tavi/cilideafricaneb/demo/service/SpeciesService.java
+++ ciclideAfricaneB/src/main/java/com/tavi/cilideafricaneb/demo/service/SpeciesService.java
@@ -1,21 +1,18 @@
 package com.tavi.cilideafricaneb.demo.service;
 
 import com.tavi.cilideafricaneb.demo.persistance.dto.SpeciesDto;
-import com.tavi.cilideafricaneb.demo.persistance.files.ResponseFile;
 import com.tavi.cilideafricaneb.demo.persistance.model.SpeciesModel;
 import com.tavi.cilideafricaneb.demo.repository.SpeciesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class SpeciesService {
@@ -23,67 +20,79 @@ public class SpeciesService {
     private SpeciesRepository speciesRepository;
 
 
-    public void save(SpeciesDto speciesDto){
+    public void save(SpeciesDto speciesDto) {
         speciesRepository.save(getModel(speciesDto));
     }
 
-    public void update(SpeciesDto speciesDto){
+    public void update(SpeciesDto speciesDto) {
         Optional<SpeciesModel> speciesModelOptional = speciesRepository.findById(speciesDto.getId());
-        if (speciesModelOptional.isPresent()){
+        if (speciesModelOptional.isPresent()) {
             SpeciesModel speciesModel = getModel(speciesDto);
             speciesModel.setIdSpecies(speciesDto.getId());
             speciesRepository.save(speciesModel);
         }
     }
 
-    public List<SpeciesDto> getAll(){
+    public List<SpeciesDto> getAll() {
         List<SpeciesModel> speciesModels = speciesRepository.findAll();
         List<SpeciesDto> speciesDtos = new ArrayList<>();
-        for (SpeciesModel speciesModel: speciesModels){
+        for (SpeciesModel speciesModel : speciesModels) {
             speciesDtos.add(getDto(speciesModel));
         }
         return speciesDtos;
     }
 
-    public List<SpeciesDto> search(String keyword){
+    public List<SpeciesDto> search(String keyword) {
         System.out.println(keyword);
 
         List<SpeciesModel> speciesModels = speciesRepository.findByNameContaining(keyword);
         List<SpeciesDto> speciesDtos = new ArrayList<>();
-        for (SpeciesModel speciesModel: speciesModels){
+        for (SpeciesModel speciesModel : speciesModels) {
             speciesDtos.add(getDto(speciesModel));
         }
         return speciesDtos;
     }
 
-    public SpeciesDto getOne(long id){
+    public SpeciesDto getOne(long id) {
         Optional<SpeciesModel> speciesModelOptional = speciesRepository.findById(id);
         SpeciesDto speciesDto = new SpeciesDto();
-        if (speciesModelOptional.isPresent()){
+        if (speciesModelOptional.isPresent()) {
             SpeciesModel speciesModel = speciesModelOptional.get();
-           speciesDto = getDto(speciesModel);
+            speciesDto = getDto(speciesModel);
         }
         return speciesDto;
     }
 
-    public void delete(long id){
+//    public List<SpeciesDto> sortAndPage(int pageN,int pageS){
+//        Pageable pageable= PageRequest.of(pageN,pageS);
+//        List<SpeciesModel> speciesModels = speciesRepository.findAll(pageable).getContent();
+//        List<SpeciesDto> speciesDtos = new ArrayList<>();
+//        for (SpeciesModel speciesModel: speciesModels){
+//            speciesDtos.add(getDto(speciesModel));
+//
+//        }
+//        return speciesDtos;
+//    }
+
+
+    public void delete(long id) {
         speciesRepository.deleteById(id);
     }
 
-    private SpeciesModel getModel(SpeciesDto speciesDto){
+    private SpeciesModel getModel(SpeciesDto speciesDto) {
         SpeciesModel speciesModel = new SpeciesModel();
-            speciesModel.setAquarium(speciesDto.getAquarium());
-            speciesModel.setAssociate(speciesDto.getAssociate());
-            speciesModel.setBehavior(speciesDto.getBehavior());
-            speciesModel.setFeed(speciesDto.getFeed());
-            speciesModel.setGroup(speciesDto.getGroup());
-            speciesModel.setName(speciesDto.getName());
-            speciesModel.setSize(speciesDto.getSize());
-            speciesModel.setReproduction(speciesDto.getReproduction());
+        speciesModel.setAquarium(speciesDto.getAquarium());
+        speciesModel.setAssociate(speciesDto.getAssociate());
+        speciesModel.setBehavior(speciesDto.getBehavior());
+        speciesModel.setFeed(speciesDto.getFeed());
+        speciesModel.setGroup(speciesDto.getGroup());
+        speciesModel.setName(speciesDto.getName());
+        speciesModel.setSize(speciesDto.getSize());
+        speciesModel.setReproduction(speciesDto.getReproduction());
         return speciesModel;
     }
 
-    private SpeciesDto getDto(SpeciesModel speciesModel){
+    private SpeciesDto getDto(SpeciesModel speciesModel) {
         SpeciesDto speciesDto = new SpeciesDto();
         speciesDto.setId(speciesModel.getIdSpecies());
         speciesDto.setAquarium(speciesModel.getAquarium());
@@ -96,5 +105,17 @@ public class SpeciesService {
         speciesDto.setSize(speciesModel.getSize());
         return speciesDto;
     }
+
+    public Page<SpeciesModel> findPaginated(int pageNr, int pageSz) {
+        Pageable pageable = PageRequest.of(pageNr - 1, pageSz);
+        List<SpeciesModel> speciesModels = speciesRepository.findAll(pageable).getContent();
+        List<SpeciesDto> speciesDtos = new ArrayList<>();
+        for (SpeciesModel speciesModel: speciesModels){
+            speciesDtos.add(getDto(speciesModel));
+
+        }
+        return speciesRepository.findAll(pageable);
+    }
+
 
 }
